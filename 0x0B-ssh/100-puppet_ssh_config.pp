fbf752785db11951c74c48fda0321f2a31699c72
lulimsky <3337@holbertonschool.com>
2022-04-21 08:19:46 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,17 +9,5 @@ file_line { 'assword no auth':
 file_line { 'file_identity':
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
- # Update configuration to file config_ssh with Puppet
-
-file_line { 'assword no auth':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthentication no',
-}
-
-file_line { 'file_identity':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/holberton',
-   line   => 'IdentityFile ~/.ssh/holberton',
+    line   => 'IdentityFile ~/.ssh/shcool',
 }
